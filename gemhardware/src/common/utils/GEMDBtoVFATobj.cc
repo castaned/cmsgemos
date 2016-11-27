ad64b7ea596bebd730fd68b07f1f8ba3a25593ca
castaned <amch031182@gmail.com>
2016-11-27 15:24:18 +0300

    adding default config for VFAT2

--- gemhardware/src/common/utils/GEMDBtoVFATobj.cc
+++ gemhardware/src/common/utils/GEMDBtoVFATobj.cc
@@ -22,28 +22,51 @@ gem::hw::utils::GEMDBtoVFATobj::~GEMDBtoVFATobj()
 }
 
 
-void gem::hw::utils::GEMDBtoVFATobj::getVFATparamfromDB(gem::hw::vfat::VFAT2ControlParams &VFAT2Params, xdata::Table &VFATDB){ 
+void gem::hw::utils::GEMDBtoVFATobj::getVFATparamfromDB(gem::hw::vfat::VFAT2ControlParams &VFAT2Params, xdata::Table &VFATDB, bool_t default){ 
 
 
   std::cout<<"Enter getVFATParamfromDB"<<std::endl;
+
   unsigned long rowIndex=0;
+  if(!default){
   VFAT2Params.trigMode  = gem::hw::vfat::StringToTriggerMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR0_TRGMODE")->toString()));
   VFAT2Params.msPol     =  gem::hw::vfat::StringToMSPolarity.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR0_MSPOLARITY")->toString()));
   VFAT2Params.calPol    = gem::hw::vfat::StringToCalPolarity.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR0_CALPOLARITY")->toString()));
   VFAT2Params.calibMode = gem::hw::vfat::StringToCalibrationMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR0_CALMODE")->toString()));
   
-  // VFAT2Params.dacMode    = gem::hw::vfat::StringToDACMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_DACSELECT")->toString()));
-  // VFAT2Params.probeMode  = gem::hw::vfat::StringToProbeMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_PROBEMODE")->toString()));
-  // VFAT2Params.lvdsMode   = gem::hw::vfat::StringToLVDSPowerSave.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_LVDSPWRSAV")->toString()));
-  // VFAT2Params.reHitCT    = gem::hw::vfat::StringToReHitCT.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_REHITCT")->toString()));
+  VFAT2Params.dacMode    = gem::hw::vfat::StringToDACMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_DACSELECT")->toString()));
+  VFAT2Params.probeMode  = gem::hw::vfat::StringToProbeMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_PROBEMODE")->toString()));
+  VFAT2Params.lvdsMode   = gem::hw::vfat::StringToLVDSPowerSave.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_LVDSPWRSAV")->toString()));
+  VFAT2Params.reHitCT    = gem::hw::vfat::StringToReHitCT.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR1_REHITCT")->toString()));
   
-  // VFAT2Params.hitCountMode = gem::hw::vfat::StringToHitCountMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_HITCNTSEL")->toString()));
-  // VFAT2Params.msPulseLen   = gem::hw::vfat::StringToMSPulseLength.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_MSPLSLEN")->toString()));
-  // VFAT2Params.digInSel     = gem::hw::vfat::StringToDigInSel.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_DIGINSEL")->toString()));
+  VFAT2Params.hitCountMode = gem::hw::vfat::StringToHitCountMode.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_HITCNTSEL")->toString()));
+  VFAT2Params.msPulseLen   = gem::hw::vfat::StringToMSPulseLength.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_MSPLSLEN")->toString()));
+  VFAT2Params.digInSel     = gem::hw::vfat::StringToDigInSel.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR2_DIGINSEL")->toString()));
   
-  // VFAT2Params.trimDACRange = gem::hw::vfat::StringToTrimDACRange.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_TRIMDACRNG")->toString()));
-  // VFAT2Params.padBandGap   = gem::hw::vfat::StringToPbBG.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_PB_BNDGAP")->toString()));
-  // VFAT2Params.sendTestPattern = gem::hw::vfat::StringToDFTestPattern.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_DFTST")->toString()));
+  VFAT2Params.trimDACRange = gem::hw::vfat::StringToTrimDACRange.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_TRIMDACRNG")->toString()));
+  VFAT2Params.padBandGap   = gem::hw::vfat::StringToPbBG.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_PB_BNDGAP")->toString()));
+  VFAT2Params.sendTestPattern = gem::hw::vfat::StringToDFTestPattern.at(boost::to_upper_copy(VFATDB.getValueAt(rowIndex,"CR3_DFTST")->toString()));
+  }
+  else{
+    VFAT2Params.trigMode  = gem::hw::vfat::StringToTriggerMode.at(boost::to_upper_copy("GEMMODE"));
+    VFAT2Params.msPol     =  gem::hw::vfat::StringToMSPolarity.at(boost::to_upper_copy("NEGATIVE"));
+    VFAT2Params.calPol    = gem::hw::vfat::StringToCalPolarity.at(boost::to_upper_copy("NEGATIVE"));
+    VFAT2Params.calibMode = gem::hw::vfat::StringToCalibrationMode.at(boost::to_upper_copy("NORMAL"));
+    
+    VFAT2Params.dacMode    = gem::hw::vfat::StringToDACMode.at(boost::to_upper_copy("OFF"));
+    VFAT2Params.probeMode  = gem::hw::vfat::StringToProbeMode.at(boost::to_upper_copy("OFF"));
+    VFAT2Params.lvdsMode   = gem::hw::vfat::StringToLVDSPowerSave.at(boost::to_upper_copy("OFF"));
+    VFAT2Params.reHitCT    = gem::hw::vfat::StringToReHitCT.at(boost::to_upper_copy("6.4MICROSEC"));
+    
+    VFAT2Params.hitCountMode = gem::hw::vfat::StringToHitCountMode.at(boost::to_upper_copy("FASTOR"));
+    VFAT2Params.msPulseLen   = gem::hw::vfat::StringToMSPulseLength.at(boost::to_upper_copy("3"));
+    VFAT2Params.digInSel     = gem::hw::vfat::StringToDigInSel.at(boost::to_upper_copy("DIGIP"));
+    
+    VFAT2Params.trimDACRange = gem::hw::vfat::StringToTrimDACRange.at(boost::to_upper_copy("0"));
+    VFAT2Params.padBandGap   = gem::hw::vfat::StringToPbBG.at(boost::to_upper_copy("OFF"));
+    VFAT2Params.sendTestPattern = gem::hw::vfat::StringToDFTestPattern.at(boost::to_upper_copy("OFF"));
+  }
+  
 
   // std::cout<<"Finishing the CR0...CR3  "<<std::endl;
 
@@ -57,15 +80,26 @@ void gem::hw::utils::GEMDBtoVFATobj::getVFATparamfromDB(gem::hw::vfat::VFAT2Cont
   xdata::Serializable* xds_BIASLATENCY     = VFATDB.getValueAt(rowIndex,"BIAS_LATENCY");
   xdata::Serializable* xds_BIASVTHRESHOLD1 = VFATDB.getValueAt(rowIndex,"BIAS_VTHRESHOLD1");
   xdata::Serializable* xds_BIASVTHRESHOLD2 = VFATDB.getValueAt(rowIndex,"BIAS_VTHRESHOLD2");
-  
+
+  if(!default){
   VFAT2Params.latency = static_cast<uint8_t>(std::stoi(xds_BIASLATENCY->toString()));
   VFAT2Params.iPreampIn = static_cast<uint8_t>(std::stoi(xds_BIASIPREAMPIN->toString()));
   VFAT2Params.iPreampFeed = static_cast<uint8_t>(std::stoi(xds_BIASIPREAMPFEED->toString()));
   VFAT2Params.iPreampOut = static_cast<uint8_t>(std::stoi(xds_BIASIPREAMPOUT->toString()));
   VFAT2Params.iShaper = static_cast<uint8_t>(std::stoi(xds_BIASISHAPER->toString()));
   VFAT2Params.iComp = static_cast<uint8_t>(std::stoi(xds_BIASICOMP->toString()));
-  
   VFAT2Params.vThresh1 = static_cast<uint8_t>(std::stoi(xds_BIASVTHRESHOLD1->toString()));
   VFAT2Params.vThresh2 = static_cast<uint8_t>(std::stoi(xds_BIASVTHRESHOLD2->toString()));
+  }
+  else{
+  VFAT2Params.latency = static_cast<uint8_t>(157);
+  VFAT2Params.iPreampIn = static_cast<uint8_t>(168);
+  VFAT2Params.iPreampFeed = static_cast<uint8_t>(80);
+  VFAT2Params.iPreampOut = static_cast<uint8_t>(150);
+  VFAT2Params.iShaper = static_cast<uint8_t>(150);
+  VFAT2Params.iComp = static_cast<uint8_t>(75);
+  VFAT2Params.vThresh1 = static_cast<uint8_t>(25);
+  VFAT2Params.vThresh2 = static_cast<uint8_t>(0);
+  }
 
 }
